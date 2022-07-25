32dac5c4a5b3ba526d065364accae1ff7b8ef0a9
Roshan Gunarathna <roshanrp.gunarathna@gmail.com>
2022-07-25 23:32:44 +0530

    Add models

---
+++ ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int CategoryId { get; set; }
+        public int Amount { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public Category Category { get; set; }
+    }
+}
